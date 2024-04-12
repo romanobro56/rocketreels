a497685c5b114312a0292b3e14feefb50c441524
Roman Pisani <romanp2929@gmail.com>
2024-04-12 12:45:44 -0400

    generate_ken_burns_command

--- editing/video_processing.py
+++ editing/video_processing.py
@@ -12,25 +12,25 @@ class VideoProcessor:
     self.editing_options = editing_options
     self.subtitle_processor = SubtitleProcessor()
 
-  # def generate_video_from_images(self, content_package, output_path):
-  #   generated_clips = []
+  def generate_video_from_images(self, content_package, output_path):
+    generated_clips = []
 
-  #   image_timestamps = self.subtitle_processor.get_image_timestamps(content_package.get_subtitles(), content_package.get_transcript_array())
-  #   for i, img_stamp in enumerate(image_timestamps):
-  #     start = img_stamp["start"]
-  #     end = img_stamp["end"]
-  #     image = Image.open(content_package.get_image_file_paths()+f"/dalle_image_{i}.jpg")
-  #     image = image.resize(self.editing_options.get_resolution())
-  #     np_img = np.array(image)
+    image_timestamps = self.subtitle_processor.get_image_timestamps(content_package.get_subtitles(), content_package.get_transcript_array())
+    for i, img_stamp in enumerate(image_timestamps):
+      start = img_stamp["start"]
+      end = img_stamp["end"]
+      image = Image.open(content_package.get_image_file_paths()+f"/dalle_image_{i}.jpg")
+      image = image.resize(self.editing_options.get_resolution())
+      np_img = np.array(image)
 
-  #     clip = ImageClip(np_img, duration=end-start)
-  #     generated_clips.append(clip)
+      clip = ImageClip(np_img, duration=end-start)
+      generated_clips.append(clip)
 
-  #   final_clip = concatenate_videoclips(generated_clips)
-  #   if not os.path.exists(output_path):
-  #     os.makedirs(output_path)
-  #   final_clip.write_videofile(output_path + "/output.mp4",fps=self.editing_options.get_frame_rate(), codec="libx264", )
-  #   return
+    final_clip = concatenate_videoclips(generated_clips)
+    if not os.path.exists(output_path):
+      os.makedirs(output_path)
+    final_clip.write_videofile(output_path + "/output.mp4",fps=self.editing_options.get_frame_rate(), codec="libx264", )
+    return
 
   def generate_video_from_images(self, content_package, output_path):
     subtitles = content_package.get_subtitles()
@@ -51,8 +51,8 @@ class VideoProcessor:
     """
     output_width = self.editing_options.get_resolution()[0]
     output_height = self.editing_options.get_resolution()[1]
-    zoom = self.editing_options.get_zoom()
     frames = self.editing_options.get_frame_rate() * duration
+    zoom = (self.editing_options.get_zoom()[1] - self.editing_options.get_zoom()[0]) / frames;
     input_width = Image.open(image_path).size[0]
     input_height = Image.open(image_path).size[1]
 
@@ -69,60 +69,26 @@ class VideoProcessor:
       possible_directions = ["R", "L", "T", "B", "C", "TR", "TL", "BR", "BL"]
 
     direction = random.choice(possible_directions)
+    zoompan = ""
 
     match direction:
-      case "R":
-        pass
-      case "L":
-        pass
-      case "T":
-        pass
-      case "B":
-        pass
-      case "C":
-        pass
-      case "TR":
-        pass
-      case "TL":
-        pass
       case "BR":
-        pass
+        zoompan=f'z=\'zoom+{zoom}\':x=\'{input_width}-{input_width}/zoom\':y=\'{input_height}-{input_height}/zoom\':d={frames}:s={output_width}x{output_height}'
       case "BL":
-        pass
+        zoompan=f'z=\'zoom+{zoom}\':x=\'0\':y=\'{input_width}-{input_height}/zoom\':d={frames}:s={output_width}x{output_height}'
+      case "TR":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'{input_width}-{input_width}/zoom\':y=\'0\':d={frames}:s={output_width}x{output_height}'
+      case "TL":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'0\':y=\'0\':d={frames}:s={output_width}x{output_height}'
+      case "R":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'{input_width}-{input_width}/zoom\':y=\'({input_height}-{output_height}/zoom)/2\':d={frames}:s={output_width}x{output_height}'
+      case "L":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'0\':y=\'({input_height}-{output_height}/zoom)/2\':d={frames}:s={output_width}x{output_height}'
+      case "T":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'({input_width}-{output_width}/zoom)/2\':y=\'0\':d={frames}:s={output_width}x{output_height}'
+      case "B":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'({input_width}-{output_width}/zoom)/2\':y=\'{input_height}-{input_height}/zoom\':d={frames}:s={output_width}x{output_height}'
+      case "C":
+        zoompan=f'z=\'zoom+{zoom}\':x=\'({input_width}-{output_width}/zoom)/2\':y=\'({input_height}-{output_height}/zoom)/2\':d={frames}:s={output_width}x{output_height}'
 
-
-
-    return f"ffmpeg -loop 1 -i {image_path} -vf \"zoompan=z='{zoom[0]}':d=125:x='{pan[0] * resolution[0]}':y='{pan[1] * resolution[1]}'\" -t 5 -s {resolution[0]}x{resolution[1]} {output_video_path}"
-
-
-    
-
-  def ken_burns_effect(self, image_path, duration, start_zoom=1.0, end_zoom=1.2, start_position=(0.5, 0.5), end_position=(0.5, 0.5)):
-
-    clip = ImageClip(image_path, duration=duration)
-
-    start_crop_size = (clip.w / start_zoom, clip.h / start_zoom)
-    end_crop_size = (clip.w / end_zoom, clip.h / end_zoom)
-
-    kb_effect = vfx.crop(clip, x_center=clip.w * start_position[0], y_center=clip.h * start_position[1], width=start_crop_size[0], height=start_crop_size[1])
-    kb_effect = kb_effect.resize(newsize=(clip.w, clip.h))
-    kb_effect = kb_effect.set_position((clip.w * (1 - start_zoom) / 2, clip.h * (1 - start_zoom) / 2))
-    kb_effect = kb_effect.crossfadein(0.5)
-
-    return kb_effect.set_duration(duration)
-  
-
-  def create_zoom_effect(input_image, output_video):
-    command = [
-      'ffmpeg', '-i', input_image,
-      '-filter_complex', "zoompan=z='zoom+0.002':d=25*4:s=1280x800",
-      '-pix_fmt', 'yuv420p', '-c:v', 'libx264', output_video
-    ]
-    
-    try:
-      # Execute the FFmpeg command
-      subprocess.run(command, check=True)
-      print(f"Zoom effect applied successfully: {output_video}")
-    except subprocess.CalledProcessError as e:
-      # Handle errors during the process
-      print(f"Error applying zoom effect: {e}")
+    return f'ffmpeg -i {image_path} -filter_complex \"zoompan={zoompan}\" -pix_fmt yuv420p -c:v libx264 {output_video_path}'
